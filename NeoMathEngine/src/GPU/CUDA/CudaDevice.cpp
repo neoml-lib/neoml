3228770b5cb8f915f7e5e31b3b0d07e1c343b3cb
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2020-06-17 23:25:57 +0300

    Add CUDA support for Linux
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoMathEngine/src/GPU/CUDA/CudaDevice.cpp
+++ NeoMathEngine/src/GPU/CUDA/CudaDevice.cpp
@@ -1,4 +1,4 @@
-/* Copyright © 2017-2020 ABBYY Production LLC
+/* Copyright Â© 2017-2020 ABBYY Production LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -21,6 +21,16 @@ limitations under the License.
 #ifdef NEOML_USE_CUDA
 
 #include <CudaDevice.h>
+#include <MathEngineAllocator.h>
+#include <MathEngineCommon.h>
+
+#include <sstream>
+#include <iostream>
+#if FINE_PLATFORM(FINE_LINUX)
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#endif
 
 namespace NeoML {
 
@@ -59,10 +69,121 @@ CCudaDevice::~CCudaDevice()
 {
 	for( int i = 0; i < CUDA_DEV_SLOT_COUNT; ++i ) {
 		if( Handles[i] != 0 ) {
-			::CloseHandle( Handles[i] );
+			ReleaseDeviceSlot( Handles[i], DeviceId, i );
 		}
 	}
 }
+
+#if FINE_PLATFORM(FINE_WINDOWS)
+
+typedef basic_string<wchar_t, char_traits<wchar_t>, CrtAllocator<wchar_t> > fstring;
+typedef basic_stringstream<wchar_t, char_traits<wchar_t>, CrtAllocator<wchar_t> > fstringstream;
+
+static fstring getCudaMutexName(int devNum, int slotNum)
+{
+	fstringstream ss;
+	ss << L"Global\\AbbyyNeoMLCudaDev" << devNum << L"_" << slotNum;
+	return ss.str();
+}
+
+bool IsDeviceSlotFree( int deviceId, int slotIndex )
+{
+	HANDLE devHandle = ::OpenMutexW( SYNCHRONIZE, FALSE, getCudaMutexName(deviceId, slotIndex).c_str() );
+	if( devHandle != 0 ) {
+		::CloseHandle(devHandle);
+		return false;
+	}
+	return true;
+}
+
+void* CaptureDeviceSlot( int deviceId, int slotIndex, bool reuse )
+{
+	void* handle = ::CreateMutexW( 0, FALSE, getCudaMutexName(deviceId, slotIndex).c_str() );
+	if( handle != nullptr && GetLastError() == ERROR_ALREADY_EXISTS && !reuse ) {
+		// Reusing slots is not allowed.
+		ReleaseDeviceSlot( handle, deviceId, slotIndex );
+		handle = nullptr;
+	}
+	return handle;
+}
+
+void ReleaseDeviceSlot( void* slot, int /*deviceId*/, int /*slotIndex*/ )
+{
+	::CloseHandle( slot );
+}
+
+#elif FINE_PLATFORM(FINE_LINUX)
+
+static const int semInitValue = 255;
+
+typedef basic_string<char, char_traits<char>, CrtAllocator<char> > fstring;
+typedef basic_stringstream<char, char_traits<char>, CrtAllocator<char> > fstringstream;
+
+static fstring getCudaMutexName(int devNum, int slotNum)
+{
+	fstringstream ss;
+	ss << "/AbbyyNeoMLCudaDev" << devNum << "_" << slotNum;
+	return ss.str();
+}
+
+bool IsDeviceSlotFree( int deviceId, int slotIndex )
+{
+	const fstring name = getCudaMutexName(deviceId, slotIndex);
+	sem_t* semaphore = ::sem_open( name.c_str(), O_CREAT, 0666, semInitValue );
+	if( semaphore != nullptr ) {
+		// Checking its value.
+		int value = 0;
+		bool isFree = false;
+		ASSERT_EXPR( ::sem_getvalue( semaphore, &value ) == 0 );
+		isFree = ( value == semInitValue );
+		::sem_close( semaphore );
+		if( isFree ) {
+			// Semaphore was free, removing it from the system.
+			::sem_unlink( name.c_str() );
+		}
+		return isFree;
+	}
+	return false;
+}
+
+void* CaptureDeviceSlot( int deviceId, int slotIndex, bool reuse )
+{
+	sem_t* semaphore = ::sem_open( getCudaMutexName(deviceId, slotIndex).c_str(),
+		O_CREAT, 0666, semInitValue );
+	if( semaphore != nullptr ) {
+		if( !reuse ) {
+			// Checking its value.
+			int value = 0;
+			bool isFree = false;
+			ASSERT_EXPR( ::sem_getvalue( semaphore, &value ) == 0 );
+			isFree = ( value == semInitValue );
+			if( !isFree ) {
+				::sem_close( semaphore );
+				// Semaphore isn't free. Skipping sem_unlink...
+				return nullptr;
+			}
+		}
+		ASSERT_EXPR( ::sem_wait( semaphore ) == 0 );
+	}
+	return semaphore;
+}
+
+void ReleaseDeviceSlot( void* slot, int deviceId, int slotIndex )
+{
+	sem_t* semaphore = static_cast<sem_t*>( slot );
+	ASSERT_EXPR( ::sem_post( semaphore ) == 0 );
+	int value = 0;
+	ASSERT_EXPR( ::sem_getvalue( semaphore, &value ) == 0 );
+	::sem_close( semaphore );
+	if( value == semInitValue ) {
+		// That was last sem_close. Removing semaphore from the system.
+		::sem_unlink( getCudaMutexName(deviceId, slotIndex).c_str() );
+	}
+}
+
+#else
+#error "Platform is not supported!"
+#endif
 
 } // namespace NeoML
 
