9fffcef863347aa700ec066e51318d1c9f878f38
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2020-08-12 14:33:32 +0300

    Remove common.h from headers
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoML/include/NeoML/Dnn/Layers/MatrixMultiplicationLayer.h
+++ NeoML/include/NeoML/Dnn/Layers/MatrixMultiplicationLayer.h
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 --------------------------------------------------------------------------------------------------------------*/
 
-#include <common.h>
 #pragma once
 
 #include <NeoML/NeoMLDefs.h>
