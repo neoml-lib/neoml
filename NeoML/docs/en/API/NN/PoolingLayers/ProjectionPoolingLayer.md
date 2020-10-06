cc84135d5a9effb1e7f70c285fc9ae2e822f0435
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2020-10-06 18:20:23 +0300

    Add CProjectionPoolingLayer (#119)
    * Add CProjectionPoolingLayer
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Remove unused variables
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Add CProjectionPoolingLayer header to the pulic interface
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Fix CProjectionPoolingLayer directions' comments
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Fix memory leak in CProjectionPoolingLayer
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Fix CProjectionPoolingLayer comment
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Add CProjectionPoolingLayer docs
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Refactor CProjectionPoolingLayer
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Rename some variables
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Update CProjectPoolingLayer doc
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Fix after reviewing code
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Fix uninitialized value warning
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Increase NeoMLTest version
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Fix cast warning in VulkanDll.cpp
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Increase NeoMLTest version
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Add override specifier to CProjectionPoolingLayer's destructor
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    Co-authored-by: Stanislav Angeliuk <59917951+SAngeliuk@users.noreply.github.com>

---
+++ NeoML/docs/en/API/NN/PoolingLayers/ProjectionPoolingLayer.md
@@ -0,0 +1,57 @@
+# CProjectionPoolingLayer Class
+
+<!-- TOC -->
+
+- [CProjectionPoolingLayer Class](#cprojectionpoolinglayer-class)
+    - [Settings](#settings)
+        - [Projection dimension](#projection-dimension)
+        - [Filter stride](#restore-original-image-size)
+    - [Trainable parameters](#trainable-parameters)
+    - [Inputs](#inputs)
+    - [Outputs](#Outputs)
+
+<!-- /TOC -->
+
+This class implements a layer which performs mean pooling over one of the blob dimensions.
+
+## Settings
+
+### Projection dimension
+
+```c++
+// Projection dimension
+void SetDimenion( TBlobDim dimension );
+```
+
+The default value is `BD_Width`.
+
+### Restore original image size
+
+```c++
+void SetRestoreOriginalImageSize( bool flag );
+```
+
+If `true` then output blob will be of the same size as input blob, and mean values will be broadcasted along pooling dimension.
+If `false` then output blob size along pooling dimension will be `1`.
+The default value is `false`.
+
+## Trainable parameters
+
+The layer has no trainable parameters.
+
+## Inputs
+
+The single input accepts a [blob](../DnnBlob.md) of the following dimensions:
+
+- `BatchLength * BatchWidth * ListSize` - the number of images in the set
+- `Height` - images' height
+- `Width` - images' width
+- `Depth * Channels` - the number of channels the image format uses
+
+## Outputs
+
+The single output contains a blob with the results.
+
+If `GetRestoreOriginalImageSize` is `true` then output is of the same size as input.
+
+If `GetRestoreOriginalImageSize` is `false` then the projection dimension of output size is equal to `1` and the rest of the dimensions are equal to the ones of the input.
---
+++ NeoML/include/NeoML/Dnn/Layers/ProjectionPoolingLayer.h
@@ -0,0 +1,64 @@
+﻿/* Copyright © 2017-2020 ABBYY Production LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+--------------------------------------------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <NeoML/NeoMLDefs.h>
+#include <NeoML/Dnn/Dnn.h>
+
+namespace NeoML {
+
+// CProjectionPoolingLayer implements a layer that calculating average along one of the blob axis
+class NEOML_API CProjectionPoolingLayer : public CBaseLayer {
+	NEOML_DNN_LAYER( CProjectionPoolingLayer )
+public:
+	explicit CProjectionPoolingLayer( IMathEngine& mathEngine );
+	~CProjectionPoolingLayer() override;
+
+	// Projection dimension
+	// BD_Width by default
+	TBlobDim GetDimension() const { return dimension; }
+	void SetDimenion( TBlobDim dimension );
+
+	// If true then output size is equal to the input size and pooling result is broadcasted along the projection dimension
+	// Otherwise projection dimension of the output is equal to 1 and other dimensions are equal to ones of the input
+	// false by default
+	bool GetRestoreOriginalImageSize() const { return restoreOriginalImageSize; }
+	void SetRestoreOriginalImageSize( bool flag );
+
+protected:
+	// CBaseLayer methods
+	void Serialize( CArchive& archive ) override;
+	void Reshape() override;
+	void RunOnce() override;
+	void BackwardOnce() override;
+
+private:
+	// Projection dimension
+	TBlobDim dimension;
+	// Does layer preserve input blob's shape?
+	bool restoreOriginalImageSize;
+
+	// Temporary blob for pool results
+	CPtr<CDnnBlob> projectionResultBlob;
+
+	// Pooling descriptor
+	CMeanPoolingDesc* desc;
+
+	void initDesc( const CBlobDesc& inputDesc );
+	void destroyDesc();
+};
+
+} // namespace NeoML
