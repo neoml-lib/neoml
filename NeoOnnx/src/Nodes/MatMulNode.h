9785806cba31ee21ee6be6edef2110de7a596493
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2020-10-29 15:23:59 +0300

    Add CMatMulNode
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

---
+++ NeoOnnx/src/Nodes/MatMulNode.h
@@ -0,0 +1,34 @@
+/* Copyright © 2017-2020 ABBYY Production LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+--------------------------------------------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "../Node.h"
+
+namespace NeoOnnx {
+
+// MatMul operator graph node
+class CMatMulNode : public COpNode {
+public:
+	CMatMulNode( int nodeIndex, const onnx::NodeProto& matMul, int opsetVersion );
+
+	// CNode methods' realizations
+	void CalcOutputTensors( CTensorCache& tensors, IMathEngine& mathEngine ) override;
+	void LabelTensorDims( const CTensorCache& tensors, CDimCache& dims ) override;
+	void AddLayers( const CGraph& graph, const CTensorCache& tensors, const CDimCache& dims,
+		CNeoMLLinkCache& neoMLLinks, CDnn& dnn ) override;
+};
+
+} // namespace NeoOnnx
