974c5d61baf054428acdf09395adee9a83c4a17b
sansanch5 <a.a.pakhomoff@gmail.com>
2020-12-30 17:31:55 +0300

    include proxy headers in the top level header
    Signed-off-by: sansanch5 <a.a.pakhomoff@gmail.com>

--- NeoML/include/NeoML/NeoML.h
+++ NeoML/include/NeoML/NeoML.h
@@ -27,10 +27,10 @@ limitations under the License.
 #include <NeoML/TraditionalML/EMClustering.h>
 #include <NeoML/TraditionalML/HierarchicalClustering.h>
 #include <NeoML/TraditionalML/MemoryProblem.h>
-#include <NeoML/TraditionalML/Linear.h>
-#include <NeoML/TraditionalML/DecisionTree.h>
+#include <NeoML/TraditionalML/LinearBinaryClassifierBuilder.h>
+#include <NeoML/TraditionalML/DecisionTreeTrainingModel.h>
 #include <NeoML/TraditionalML/OneVersusAll.h>
-#include <NeoML/TraditionalML/Svm.h>
+#include <NeoML/TraditionalML/SvmBinaryClassifierBuilder.h>
 #include <NeoML/TraditionalML/PlattScalling.h>
 #include <NeoML/TraditionalML/DifferentialEvolution.h>
 #include <NeoML/Dnn/DnnBlob.h>
