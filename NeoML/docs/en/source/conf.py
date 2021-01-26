f6a9679941a44b61fde0b2bb971ec55537d5dd23
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2021-01-26 15:25:14 +0300

    Fix .readthedocs.yml
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoML/docs/en/source/conf.py
+++ NeoML/docs/en/source/conf.py
@@ -86,8 +86,6 @@ for dirpath, _, filenames in walk('.'):
 
 # -- General configuration ---------------------------------------------------
 
-# import neoml
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
