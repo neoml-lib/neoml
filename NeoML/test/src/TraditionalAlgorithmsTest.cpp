5587e7e9cea456ee11d8951510d48c4013bb9527
sansanch5 <a.a.pakhomoff@gmail.com>
2021-01-28 13:37:23 +0300

    cover all traditional classification algorithms
    Signed-off-by: sansanch5 <a.a.pakhomoff@gmail.com>

--- NeoML/test/src/TraditionalAlgorithmsTest.cpp
+++ NeoML/test/src/TraditionalAlgorithmsTest.cpp
@@ -24,18 +24,18 @@ limitations under the License.
 using namespace NeoML;
 using namespace NeoMLTest;
 
-class CSvmClassificationTest : public CNeoMLTestFixture {
+class RandomDense4000x20 : public CNeoMLTestFixture {
 public:
 	static bool InitTestFixture(); // Инициализация теста
 	static void DeinitTestFixture(); // Деинициализация теста
 };
 
-bool CSvmClassificationTest::InitTestFixture()
+bool RandomDense4000x20::InitTestFixture()
 {
 	return true;
 }
 
-void CSvmClassificationTest::DeinitTestFixture()
+void RandomDense4000x20::DeinitTestFixture()
 {
 }
 
@@ -74,7 +74,8 @@ public:
 
 		std::random_device rd;
 		std::mt19937 gen( rd() );
-		std::uniform_real_distribution<float> df( std::numeric_limits<float>::min(), std::numeric_limits<float>::max() );
+		//std::uniform_real_distribution<float> df( std::numeric_limits<float>::min(), std::numeric_limits<float>::max() );
+		std::uniform_real_distribution<float> df( -10, 10 );
 		std::uniform_int_distribution<int> di( 0, classes - 1 );
 		res->valuesArr.SetBufferSize( samples * features );
 		res->classesArr.SetBufferSize( samples );
@@ -117,9 +118,82 @@ CPtr<CDenseMemoryProblem> DenseBinaryProblem = CDenseMemoryProblem::Random( 4000
 
 //------------------------------------------------------------------------------------------------
 
-TEST_F( CSvmClassificationTest, RandomDense4000x20 )
+TEST_F( RandomDense4000x20, Svm )
 {
 	CSvmBinaryClassifierBuilder::CParams params( CSvmKernel::KT_Linear );
 	CSvmBinaryClassifierBuilder svm( params );
 	auto model = svm.Train( *DenseBinaryProblem );
 }
+
+TEST_F( RandomDense4000x20, Linear )
+{
+	CLinearBinaryClassifierBuilder::CParams params( EF_SquaredHinge );
+	params.L1Coeff = 0.05f;
+	CLinearBinaryClassifierBuilder linear( params );
+	auto model = linear.Train( *DenseBinaryProblem );
+}
+
+TEST_F( RandomDense4000x20, DecisionTree )
+{
+	CDecisionTreeTrainingModel::CParams param;
+	param.MaxTreeDepth = 10;
+	param.MaxNodesCount = 4096;
+	param.AvailableMemory = Megabyte;
+	CDecisionTreeTrainingModel decisionTree( param );
+	auto model = decisionTree.TrainModel<IDecisionTreeModel>( *DenseBinaryProblem );
+}
+
+TEST_F( RandomDense4000x20, GradientBoosting )
+{
+	int begin = GetTickCount();
+
+	CRandom random;
+	GTEST_LOG_( INFO ) << "Random = " << random.Next();
+	CGradientBoost::CParams params;
+	params.Random = &random;
+	params.LossFunction = CGradientBoost::LF_Binomial;
+	params.IterationsCount = 10;
+	params.LearningRate = 0.3f;
+	params.MaxTreeDepth = 8;
+	params.ThreadCount = 1;
+	params.Subsample = 1;
+	params.Subfeature = 1;
+	params.MinSubsetWeight = 8;
+
+	CGradientBoost boosting( params );
+	auto model = boosting.TrainModel<IGradientBoostModel>( *DenseBinaryProblem );
+
+	GTEST_LOG_( INFO ) << "Train time: " << GetTickCount() - begin;
+	GTEST_LOG_( INFO ) << "The last loss: " << boosting.GetLastLossMean();
+}
+
+TEST_F( RandomDense4000x20, CrossValidation )
+{
+	CLinearBinaryClassifierBuilder::CParams params( EF_SquaredHinge );
+	CLinearBinaryClassifierBuilder linear( params );
+
+	CCrossValidation crossValidation( linear, DenseBinaryProblem );
+	const int PartsCount = 10;
+	CCrossValidationResult result;
+	crossValidation.Execute( PartsCount, AccuracyScore, result, true );
+
+	ASSERT_EQ( result.Models.Size(), PartsCount );
+	ASSERT_EQ( result.Success.Size(), PartsCount );
+	ASSERT_EQ( result.Results.Size(), DenseBinaryProblem->GetVectorCount() );
+
+	CSigmoid sigmoid;
+	CalcSigmoidCoefficients( result, sigmoid );
+
+	params.SigmoidCoefficients = sigmoid;
+	CLinearBinaryClassifierBuilder builderS( params );
+
+	auto model = builderS.Train( *DenseBinaryProblem );
+}
+
+TEST_F( RandomDense4000x20, OneVsAll )
+{
+	CLinearBinaryClassifierBuilder linear( EF_SquaredHinge );
+	COneVersusAll ovaLinear( linear );
+	auto model = ovaLinear.TrainModel<NeoML::IOneVersusAllModel>( *DenseBinaryProblem );
+}
+
