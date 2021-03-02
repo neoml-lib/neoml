e064f75b461b73e1df7bf87177534271d213b2f7
valinurovdenis <denis.valinurow@yandex.ru>
2021-03-02 18:45:20 +0300

    layers have no output

--- NeoML/src/Dnn/Layers/CtcLayer.cpp
+++ NeoML/src/Dnn/Layers/CtcLayer.cpp
@@ -92,6 +92,7 @@ void CCtcLossLayer::Reshape()
 {
 	CheckInputs();
 
+	CheckArchitecture( outputDescs.IsEmpty(), GetName(), "CCtcLossLayer has no output" );
 	CheckArchitecture(!GetDnn()->IsRecurrentMode(),
 		GetName(), "ctc loss layer inside the recurrent composite layer" );
 	CheckArchitecture( GetInputCount() >= 2 && GetInputCount() <= 5,
@@ -574,6 +575,7 @@ CCtcDecodingLayer::CCtcDecodingLayer( IMathEngine& mathEngine ) :
 void CCtcDecodingLayer::Reshape()
 {
 	CheckInputs();
+	CheckArchitecture( outputDescs.IsEmpty(), GetName(), "CCtcDecodingLayer has no output" );
 	CBlobDesc transposedDesc = inputDescs[I_Result];
 	transposedDesc.SetDimSize(BD_BatchLength, inputDescs[I_Result].BatchWidth());
 	transposedDesc.SetDimSize(BD_BatchWidth, inputDescs[I_Result].BatchLength());
