9cf0c447b338c8ecb2b3d4ab1033fc06a46317a6
Nadezda Gaganova <nadezda.gaganova@abbyy.com>
2021-03-15 17:20:48 +0300

    docs for Dnn

--- NeoML/Python/neoml/Dnn/Dnn.py
+++ NeoML/Python/neoml/Dnn/Dnn.py
@@ -22,7 +22,17 @@ import neoml.Random
 
 
 class Dnn(PythonWrapper.Dnn):
-    """
+    """Neural network implementation.
+    A neural network is a directed graph consisting of layers that perform
+    calculations on data blobs. It starts with source layers and ends with 
+    sink layers.
+    
+    Parameters
+    ---------
+    math_engine : object
+        The math engine that will perform calculations.
+    random : object, default=None
+        The random numbers generator to be used for training and initialization.
     """
     def __init__(self, math_engine, random=None):
         if not isinstance(math_engine, neoml.MathEngine.MathEngine):
@@ -37,75 +47,100 @@ class Dnn(PythonWrapper.Dnn):
         super().__init__(random, math_engine._internal)
 
     def store(self, path):
-        """
+        """Serializes the network.
+        
+        Parameters
+        ---------
+        path : str
+            The full path to the location where the network should be stored.
         """
         self._store(str(path))
 
     def load(self, path):
-        """
+        """Loads the network from file.
+        
+        Parameters
+        ---------
+        path : str
+            The full path to the location from where the network should be loaded.
         """
         self._load(str(path))
 
     def store_checkpoint(self, path):
-        """
+        """Serializes the network with the data required to resume training.
+        
+        Parameters
+        ---------
+        path : str
+            The full path to the location where the network should be stored.
         """
         self._store_checkpoint(str(path))
 
     def load_checkpoint(self, path):
-        """
+        """Loads the checkpoint from file.
+        A new solver will be created, because the old pointers will point
+        to an object no longer used by this network.
+        
+        Parameters
+        ---------
+        path : str
+            The full path to the location from where the network should be loaded.
         """
         self._load_checkpoint(str(path))
 
     @property
     def math_engine(self):
-        """
+        """Gets the math engine used by the network.
         """
         return self.get_math_engine()
 
     @property
     def solver(self):
-        """
+        """Gets the optimizer for the layer's trainable parameters.
         """
         return self.get_solver()
 
     @solver.setter
     def solver(self, new_solver):
-        """
+        """Sets the optimizer for the layer's trainable parameters.
         """
         self.set_solver(new_solver._internal)
 
     @property
     def initializer(self):
-        """
+        """Gets the initializer that will fill in the weight values 
+        before training starts. Xavier initialization is the default.
         """
         return self.get_initializer()
 
     @initializer.setter
     def initializer(self, new_initializer):
-        """
+        """Sets the initializer that will fill in the weight values 
+        before training starts.
         """
         self.set_initializer(new_initializer._internal)
 
     @property
     def input_layers(self):
-        """
+        """Gets the source layers of the network.
         """
         return self.get_inputs()
 
     @property
     def output_layers(self):
-        """
+        """Gets the sink layers of the network.
         """
         return self.get_outputs()
 
     @property
     def layers(self):
-        """
+        """Gets all layers of the network.
         """
         return self.get_layers()
 
     def run(self, inputs):
         """Runs the network.
+        
         Parameters
         ----------
         inputs : The dictionary of input blobs.
@@ -135,7 +170,15 @@ class Dnn(PythonWrapper.Dnn):
         return outputs
 
     def run_and_backward(self, inputs):
-        """
+        """Runs the network and performs a backward pass with the input data.
+        
+        Parameters
+        ---------
+        inputs : The dictionary of input blobs.
+
+        Returns
+        -------
+            The list of output blobs.
         """
         dnn_inputs = self.get_inputs()
 
@@ -152,7 +195,16 @@ class Dnn(PythonWrapper.Dnn):
         return self._run_and_backward(input_list)
 
     def learn(self, inputs):
-        """
+        """Runs the network, performs a backward pass 
+        and updates the trainable weights.
+        
+        Parameters
+        ---------
+        inputs : The dictionary of input blobs.
+
+        Returns
+        -------
+            The list of output blobs.
         """
         dnn_inputs = self.get_inputs()
 
@@ -172,7 +224,7 @@ class Dnn(PythonWrapper.Dnn):
 
 
 class Layer:
-    """
+    """The base class for a network layer.
     """
     def __init__(self, internal):
         if not isinstance(internal, PythonWrapper.Layer):
@@ -182,6 +234,6 @@ class Layer:
 
     @property
     def name(self):
-        """
+        """Gets the layer name.
         """
         return self._internal.get_name()
