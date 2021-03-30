c2ec1c50a41ef95fef0c485cdc83a16817b212a0
valinurovdenis <denis.valinurow@yandex.ru>
2021-03-30 11:25:30 +0300

    feature int32
    Signed-off-by: valinurovdenis <denis.valinurow@yandex.ru>

--- NeoML/src/TraditionalML/CompactRegressionTree.h
+++ NeoML/src/TraditionalML/CompactRegressionTree.h
@@ -14,7 +14,6 @@ limitations under the License.
 --------------------------------------------------------------------------------------------------------------*/
 
 #pragma once
-#undef max
 
 #include <limits>
 #include <RegressionTree.h>
@@ -54,7 +53,7 @@ public:
 	virtual void Serialize( CArchive& archive ) override;
 
 	// Feature number cannot exceeed this.
-	static const T MaxFeature = std::numeric_limits<T>::max() - 2;
+	static const T MaxFeature = (std::numeric_limits<T>::max)() - 2;
 	// Node index within the `nodes` array cannot exceed this.
 	static const uint16_t MaxNodeIndex = UINT16_MAX - 1;
 
