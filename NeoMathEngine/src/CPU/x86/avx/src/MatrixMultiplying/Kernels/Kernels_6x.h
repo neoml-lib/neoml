023aa6ff839ce8dff6d39d7a0724f9c768ba5065
Yuri Katkov <yuri.katkov@abbyy.com>
2021-04-24 01:01:18 +0300

    Added comments. Fixed bug for kernel 6x2
    Signed-off-by: Yuri Katkov <yuri.katkov@abbyy.com>

--- NeoMathEngine/src/CPU/x86/avx/src/MatrixMultiplying/Kernels/Kernels_6x.h
+++ NeoMathEngine/src/CPU/x86/avx/src/MatrixMultiplying/Kernels/Kernels_6x.h
@@ -567,25 +567,43 @@ struct CMicroKernel_6x2 : public CMicroKernelBase<6, 2> {
 
 				__m256 b00, b01, b02, b10, b11, b12;
 				__m256 a00, a01, a02, a10, a11, a12;
+				__m256i premVar = PERMUTE8( 3, 3, 2, 2, 1, 1, 0, 0 );
 
 				// TODO: Check bPtr align
 				double const * bPtrDouble = reinterpret_cast<double const *>( bPtr );
 				__m128 const * aPtrVec = reinterpret_cast<__m128 const *>( aPtr );
 
 				for( ; k >= 4; k -= 4 ) {
+					//            b0k0 b1k0
+					//            b0k1 b1k1
+					// a0k0 a0k1  c00  c01
+					// a1k0 a1k1  c10  c11
+					// a2k0 a2k1  c20  c21
+					// a3k0 a3k1  c30  c31
+					// a4k0 a4k1  c40  c41
+					// a5k0 a5k1  c50  c51
+
+					// b: b0k0 b1k0 b0k1 b1k1 b2k0 b2k1 b3k0 b3k1
 					__m256d b = _mm256_loadu_pd( bPtrDouble );
 					bPtrDouble += 4;
 
+					// a00: a0k0 a1k0 a2k0 a3k0 a0k0 a1k0 a2k0 a3k0
 					a00 = _mm256_broadcast_ps( aPtrVec++ );
+					// a01: a4k0 a5k0 a0k1 a1k1 a4k0 a5k0 a0k1 a1k1
 					a01 = _mm256_broadcast_ps( aPtrVec++ );
+					// a02: a2k1 a3k1 a4k1 a5k1 a2k1 a3k1 a4k1 a5k1
 					a02 = _mm256_broadcast_ps( aPtrVec++ );
 
+					// b00: b0k0 b1k0 b0k1 b1k1 b0k0 b1k0 b0k1 b1k1
 					b00 = _mm256_castpd_ps( _mm256_permute2f128_pd( b, b, PERMUTE2( 0, 0 ) ) );
 					b02 = b00;
+					// b10: b2k0 b2k1 b3k0 b3k1 b2k0 b2k1 b3k0 b3k1
 					b10 = _mm256_castpd_ps( _mm256_permute2f128_pd( b, b, PERMUTE2( 1, 1 ) ) );
 					b12 = b10;
 
+					// b00: b0k0 b1k0 b0k0 b1k0 b0k0 b1k0 b0k0 b1k0
 					b00 = _mm256_castpd_ps( _mm256_shuffle_pd( _mm256_castps_pd( b00 ), _mm256_castps_pd( b00 ), SHUFFLE4( 0, 0, 0, 0 ) ) );
+					// b02: b0k1 b1k1 b0k1 b1k1 b0k1 b1k1 b0k1 b1k1
 					b02 = _mm256_castpd_ps( _mm256_shuffle_pd( _mm256_castps_pd( b02 ), _mm256_castps_pd( b02 ), SHUFFLE4( 1, 1, 1, 1 ) ) );
 					b10 = _mm256_castpd_ps( _mm256_shuffle_pd( _mm256_castps_pd( b10 ), _mm256_castps_pd( b10 ), SHUFFLE4( 0, 0, 0, 0 ) ) );
 					b12 = _mm256_castpd_ps( _mm256_shuffle_pd( _mm256_castps_pd( b12 ), _mm256_castps_pd( b12 ), SHUFFLE4( 1, 1, 1, 1 ) ) );
@@ -594,16 +612,20 @@ struct CMicroKernel_6x2 : public CMicroKernelBase<6, 2> {
 					a11 = _mm256_broadcast_ps( aPtrVec++ );
 					a12 = _mm256_broadcast_ps( aPtrVec++ );
 
+					// b10: b0k0 b1k0 b0k0 b1k0 b0k1 b1k1 b0k1 b1k1
 					b01 = _mm256_blend_ps( b00, b02, BLEND8( 1, 1, 1, 1, 0, 0, 0, 0 ) );
 					b11 = _mm256_blend_ps( b10, b12, BLEND8( 1, 1, 1, 1, 0, 0, 0, 0 ) );
 
-					a00 = _mm256_permutevar_ps( a00, PERMUTE8( 3, 3, 2, 2, 1, 1, 0, 0 ) );
-					a01 = _mm256_permutevar_ps( a01, PERMUTE8( 3, 3, 2, 2, 1, 1, 0, 0 ) );
-					a02 = _mm256_permutevar_ps( a02, PERMUTE8( 3, 3, 2, 2, 1, 1, 0, 0 ) );
-					a10 = _mm256_permutevar_ps( a10, PERMUTE8( 3, 3, 2, 2, 1, 1, 0, 0 ) );
-					a11 = _mm256_permutevar_ps( a11, PERMUTE8( 3, 3, 2, 2, 1, 1, 0, 0 ) );
-					a12 = _mm256_permutevar_ps( a12, PERMUTE8( 3, 3, 2, 2, 1, 1, 0, 0 ) );
+					// a00: a0k0 a0k0 a1k0 a1k0 a2k0 a2k0 a3k0 a3k0
+					a00 = _mm256_permutevar_ps( a00, premVar );
+					a01 = _mm256_permutevar_ps( a01, premVar );
+					a02 = _mm256_permutevar_ps( a02, premVar );
+					a10 = _mm256_permutevar_ps( a10, premVar );
+					a11 = _mm256_permutevar_ps( a11, premVar );
+					a12 = _mm256_permutevar_ps( a12, premVar );
 
+					// a00: a0k0 a0k0 a1k0 a1k0 a2k0 a2k0 a3k0 a3k0
+					// b00: b0k0 b1k0 b0k0 b1k0 b0k0 b1k0 b0k0 b1k0
 					c0 = _mm256_fmadd_ps( a00, b00, c0 );
 					c1 = _mm256_fmadd_ps( a01, b01, c1 );
 					c2 = _mm256_fmadd_ps( a02, b02, c2 );
@@ -621,9 +643,9 @@ struct CMicroKernel_6x2 : public CMicroKernelBase<6, 2> {
 					a02 = _mm256_broadcast_ps( aPtrVec++ );
 
 					b01 = _mm256_blend_ps( b00, b02, BLEND8( 1, 1, 1, 1, 0, 0, 0, 0 ) );
-					a00 = _mm256_permutevar_ps( a00, PERMUTE8( 3, 3, 2, 2, 1, 1, 0, 0 ) );
-					a01 = _mm256_permutevar_ps( a01, PERMUTE8( 3, 3, 2, 2, 1, 1, 0, 0 ) );
-					a02 = _mm256_permutevar_ps( a02, PERMUTE8( 3, 3, 2, 2, 1, 1, 0, 0 ) );
+					a00 = _mm256_permutevar_ps( a00, premVar );
+					a01 = _mm256_permutevar_ps( a01, premVar );
+					a02 = _mm256_permutevar_ps( a02, premVar );
 
 					c0 = _mm256_fmadd_ps( a00, b00, c0 );
 					c1 = _mm256_fmadd_ps( a01, b01, c1 );
@@ -631,14 +653,21 @@ struct CMicroKernel_6x2 : public CMicroKernelBase<6, 2> {
 				}
 
 				if( k == 1 ) {
+					// b00: b0k0 b1k0 b0k0 b1k0 b0k0 b1k0 b0k0 b1k0
 					b00 = _mm256_castpd_ps( _mm256_broadcast_sd( bPtrDouble++ ) );
 					b02 = _mm256_setzero_ps();
+					// a00: a0k0 a1k0 a2k0 a3k0 a0k0 a1k0 a2k0 a3k0
 					a00 = _mm256_broadcast_ps( aPtrVec++ );
-					a01 = _mm256_broadcast_ps( aPtrVec++ );
+					// a01: a4k0 a5k0 0 0 0 0 0 0
+					a01 = _mm256_maskload_ps( reinterpret_cast<float const *>( aPtrVec ), _mm256_set_epi32( 0, 0, 0, 0, 0, 0, -1, -1 ) );
 
+					// b01: b0k0 b1k0 b0k0 b1k0 0 0 0 0
 					b01 = _mm256_blend_ps( b00, b02, BLEND8( 1, 1, 1, 1, 0, 0, 0, 0 ) );
-					a00 = _mm256_permutevar_ps( a00, PERMUTE8( 3, 3, 2, 2, 1, 1, 0, 0 ) );
-					a01 = _mm256_permutevar_ps( a01, PERMUTE8( 3, 3, 2, 2, 1, 1, 0, 0 ) );
+
+					// a00: a0k0 a0k0 a1k0 a1k0 a2k0 a2k0 a3k0 a3k0
+					a00 = _mm256_permutevar_ps( a00, premVar );
+					// a00: a4k0 a4k0 a5k0 a5k0 0 0 0 0
+					a01 = _mm256_permutevar_ps( a01, premVar );
 
 					c0 = _mm256_fmadd_ps( a00, b00, c0 );
 					c1 = _mm256_fmadd_ps( a01, b01, c1 );
@@ -665,10 +694,10 @@ struct CMicroKernel_6x2 : public CMicroKernelBase<6, 2> {
 				// Get back start address
 				cPtr += cRowSize + 6;
 				mask = _mm256_set_epi32( 0, 0, 0, 0, 0, 0, -1, -1 );
-				_mm256_maskstore_ps( cPtr, mask, _mm256_add_ps ( c0, _mm256_maskload_ps( cPtr, mask ) ) );
+				_mm256_maskstore_ps( cPtr, mask, _mm256_add_ps ( c1, _mm256_maskload_ps( cPtr, mask ) ) );
 				cPtr += cRowSize - 2;
 				mask = _mm256_set_epi32( 0, 0, 0, 0, -1, -1, 0, 0 );
-				_mm256_maskstore_ps( cPtr, mask, _mm256_add_ps ( c0, _mm256_maskload_ps( cPtr, mask ) ) );
+				_mm256_maskstore_ps( cPtr, mask, _mm256_add_ps ( c1, _mm256_maskload_ps( cPtr, mask ) ) );
 	}
 };
 
@@ -769,5 +798,5 @@ struct CMicroKernel_6x1 : public CMicroKernelBase<6, 1> {
 	}
 };
 
-using CKernelCombi = CKernelCombineHorizontal<CMicroKernel_6x16, CMicroKernel_6x8, CMicroKernel_6x4, CMicroKernel_6x2, CMicroKernel_6x1>;
+using CKernelCombi = CKernelCombineHorizontal<CMicroKernel_6x16, CMicroKernel_6x8, CMicroKernel_6x4, CMicroKernel_6x2/*, CMicroKernel_6x1*/>;
 
