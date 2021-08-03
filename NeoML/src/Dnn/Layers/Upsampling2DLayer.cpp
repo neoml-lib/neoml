66eee474e294064b7352260e9ad8198151064109
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2021-08-03 13:16:48 +0300

    Support integers in CUpsampling2DLayer
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoML/src/Dnn/Layers/Upsampling2DLayer.cpp
+++ NeoML/src/Dnn/Layers/Upsampling2DLayer.cpp
@@ -66,8 +66,13 @@ void CUpsampling2DLayer::RunOnce()
 	NeoAssert( inputBlobs.Size() == outputBlobs.Size() );
 
 	for( int i = 0; i < inputBlobs.Size(); ++i ) {
-		MathEngine().Upsampling2DForward( inputBlobs[i]->GetDesc(), inputBlobs[i]->GetData(),
-			heightCopyCount, widthCopyCount, outputBlobs[i]->GetDesc(), outputBlobs[i]->GetData() );
+		if( inputBlobs[i]->GetDataType() == CT_Float ) {
+			MathEngine().Upsampling2DForward( inputBlobs[i]->GetDesc(), inputBlobs[i]->GetData(),
+				heightCopyCount, widthCopyCount, outputBlobs[i]->GetDesc(), outputBlobs[i]->GetData() );
+		} else {
+			MathEngine().Upsampling2DForward( inputBlobs[i]->GetDesc(), inputBlobs[i]->GetData<int>(),
+				heightCopyCount, widthCopyCount, outputBlobs[i]->GetDesc(), outputBlobs[i]->GetData<int>() );
+		}
 	}
 }
 
@@ -75,6 +80,7 @@ void CUpsampling2DLayer::BackwardOnce()
 {
 	NeoAssert( inputDiffBlobs.Size() == outputDiffBlobs.Size() );
 	for( int i = 0; i < inputDiffBlobs.Size(); ++i ) {
+		NeoAssert( inputBlobs[i]->GetDataType() == CT_Float );
 		MathEngine().Upsampling2DBackward( outputDiffBlobs[i]->GetDesc(), outputDiffBlobs[i]->GetData(),
 			heightCopyCount, widthCopyCount, inputDiffBlobs[i]->GetDesc(), inputDiffBlobs[i]->GetData() );
 	}
