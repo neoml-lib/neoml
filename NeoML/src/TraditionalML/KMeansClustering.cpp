061933339a073081651a701421d85da4d48870e4
NValerij <nov.valerij@gmail.com>
2021-08-12 23:50:17 +0300

    CDnnBlobBuffer do not throw exceptions from destructor. (#397)
    * CDnnBlobBuffer do not throw exceptions from destructor. Explicit Close-method added.
    
    Signed-off-by: Valeriy Novitskiy <nov.valerij@gmail.com>
    
    * Fix comments.
    
    Signed-off-by: Valeriy Novitskiy <nov.valerij@gmail.com>
    
    * Small improvements.
    
    Signed-off-by: Valeriy Novitskiy <nov.valerij@gmail.com>

--- NeoML/src/TraditionalML/KMeansClustering.cpp
+++ NeoML/src/TraditionalML/KMeansClustering.cpp
@@ -51,6 +51,7 @@ static CPtr<CDnnBlob> createWeightBlob( IMathEngine& mathEngine, const IClusteri
 	for( int vectorIndex = 0; vectorIndex < vectorCount; ++vectorIndex ) {
 		buffer[vectorIndex] = static_cast<float>( data->GetVectorWeight( vectorIndex ) );
 	}
+	buffer.Close();
 	return weight;
 }
 
@@ -653,6 +654,7 @@ void CKMeansClustering::selectInitialClusters( const CDnnBlob& data, int seed, C
 			::memcpy( currPtr, initialClusterCenters[i].Mean.GetPtr(), featureCount * sizeof( float ) );
 			currPtr += featureCount;
 		}
+		buffer.Close();
 		return;
 	}
 
@@ -875,6 +877,7 @@ void CKMeansClustering::recalcCenters( const CDnnBlob& data, const CDnnBlob& wei
 		}
 		newCenter += featureCount;
 	}
+	rawSizes.Close();
 }
 
 // Calculates clusters' variances
