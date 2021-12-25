e5b2ae8de0e17abdc7c542fb20511e26c271ddbb
Yury Katkov <ykatkov@yandex.ru>
2021-12-26 00:17:39 +0300

    Before debugging Lstm jit function
    Signed-off-by: Yury Katkov <ykatkov@yandex.ru>

--- NeoMathEngine/src/CPU/CpuMathEngine.h
+++ NeoMathEngine/src/CPU/CpuMathEngine.h
@@ -526,9 +526,9 @@ public:
 		const CFloatHandle& recurrentWeights, const CConstFloatHandle& recurrentFreeTerm,
 		const CConstFloatHandle& inputStateBackLink, const CConstFloatHandle& inputMainBackLink, const CConstFloatHandle& input,
 		const CFloatHandle& outputStateBackLink, const CFloatHandle& outputMainBackLink ) override;
-	void CalcTanh( float* dst, const float* src, size_t dataSize, bool isMultithread ) {
-		simdMathEngine->Tanh( dst, src, dataSize, isMultithread );
-	}
+	void CalcTanh( const CConstFloatHandle& src, const CFloatHandle& dst, int dataSize ) override;
+	void CalcSigmoid( const CConstFloatHandle& src, const CFloatHandle& dst, int dataSize ) override;
+	void CalcExp( const CConstFloatHandle& src, const CFloatHandle& dst, int dataSize ) override;
 
 	IPerformanceCounters* CreatePerformanceCounters() const override;
 	void SetDistributedCommunicator( std::shared_ptr<CMultiThreadDistributedCommunicator> comm, const CMathEngineDistributedInfo& info );
