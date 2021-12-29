6c558c38ff429cef94090d123b98b39304021f7f
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2021-12-30 00:54:10 +0300

    Remove ternary operator over constant value
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoMathEngine/src/GPU/Vulkan/VulkanMathEngineBlas.cpp
+++ NeoMathEngine/src/GPU/Vulkan/VulkanMathEngineBlas.cpp
@@ -774,7 +774,6 @@ void CVulkanMathEngine::MultiplyMatrixByDiagMatrix( const CConstFloatHandle& fir
 {
 	const int batchSize = 1;
 	const int secondBatchSize = 1;
-	const bool toAdd = false;
 	int matrixSize = batchSize * firstHeight * firstWidth;
 
 	ASSERT_EXPR( resultBufferSize >= matrixSize );
@@ -786,8 +785,7 @@ void CVulkanMathEngine::MultiplyMatrixByDiagMatrix( const CConstFloatHandle& fir
 		CMemoryHandle bufs[2] = { firstHandle, resultHandle };
 		size_t sizes[2] = { matrixSize * sizeof( float ), matrixSize * sizeof( float ) };
 
-		PARAM_STRUCT( MultiplyMatrixByDiagMatrixAdreno ) param =
-		{ batchSize, secondBatchSize, firstHeight, firstWidth, ( toAdd ? 1 : 0 ) };
+		PARAM_STRUCT( MultiplyMatrixByDiagMatrixAdreno ) param = { batchSize, secondBatchSize, firstHeight, firstWidth, 0 };
 
 		runShader( shaderLoader->GET_SHADER_DATA( MultiplyMatrixByDiagMatrixAdreno, true, 0, 1, 2 ),
 			&param, sizeof( param ), 0, 0, samplers, 1, bufs, sizes, 2, Ceil( firstWidth, 4 ), batchSize * firstHeight, 1 );
@@ -795,8 +793,7 @@ void CVulkanMathEngine::MultiplyMatrixByDiagMatrix( const CConstFloatHandle& fir
 		CMemoryHandle bufs[3] = { firstHandle, secondHandle, resultHandle };
 		size_t sizes[3] = { matrixSize * sizeof( float ), firstWidth * sizeof( float ), matrixSize * sizeof( float ) };
 
-		PARAM_STRUCT( MultiplyMatrixByDiagMatrix ) param =
-		{ batchSize, secondBatchSize, firstHeight, firstWidth, ( toAdd ? 1 : 0 ) };
+		PARAM_STRUCT( MultiplyMatrixByDiagMatrix ) param = { batchSize, secondBatchSize, firstHeight, firstWidth, 0 };
 
 		runShader( shaderLoader->GET_SHADER_DATA( MultiplyMatrixByDiagMatrix, true, 0, 0, 3 ),
 			&param, sizeof( param ), 0, 0, 0, 0, bufs, sizes, 3, Ceil( firstWidth, 4 ), batchSize * firstHeight, 1 );
