0a3750a109d09f8a449c7c4bb13f549ea0f87ccc
Yury Katkov <ykatkov@yandex.ru>
2022-01-25 17:08:41 +0300

    Fixed build
    Signed-off-by: Yury Katkov <ykatkov@yandex.ru>

--- NeoMathEngine/src/GPU/CUDA/CudaMathEngine.h
+++ NeoMathEngine/src/GPU/CUDA/CudaMathEngine.h
@@ -528,6 +528,13 @@ public:
 	void BertConvBackward( const CConstFloatHandle& dataHandle, const CConstFloatHandle& kernelHandle,
 		const CConstFloatHandle& outDiffHandle, int seqLen, int batchSize, int numHeads, int headSize, int kernelSize,
 		const CFloatHandle& dataDiffHandle, const CFloatHandle& kernelDiffHandle ) override;
+	CLstmDesc* InitLstm( const CFloatHandle& inputFullyConnectedResult, const CFloatHandle& reccurentFullyConnectedResult,
+		int hiddenSize, int objectCount, int objectSize ) override;
+	void Lstm( CLstmDesc& desc,
+		const CFloatHandle& inputWeights, const CConstFloatHandle& inputFreeTerm,
+		const CFloatHandle& recurrentWeights, const CConstFloatHandle& recurrentFreeTerm,
+		const CConstFloatHandle& inputStateBackLink, const CConstFloatHandle& inputMainBackLink, const CConstFloatHandle& input,
+		const CFloatHandle& outputStateBackLink, const CFloatHandle& outputMainBackLink ) override;
 
 	IPerformanceCounters* CreatePerformanceCounters() const override { 	return new CPerformanceCountersDefault(); }
 	void AllReduce( const CFloatHandle& handle, int size ) override;
