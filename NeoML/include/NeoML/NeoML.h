72990fe826e68c8082fbb2b14a4af779883826c9
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2022-01-27 23:56:23 +0300

    Extend CDistributedTraining interfacce (#550)
    * Extend CDistributedTraining interfacce
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Fix non-OMP compilation
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    * Support CCrfLossLayer as a loss in CDistributedTraining
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    Signed-off-by: Michael Zatsepin <michael.zatsepin@abbyy.com>

--- NeoML/include/NeoML/NeoML.h
+++ NeoML/include/NeoML/NeoML.h
@@ -40,6 +40,7 @@ limitations under the License.
 #include <NeoML/Dnn/AutoDiff.h>
 #include <NeoML/Dnn/AutoDiffFunctions.h>
 #include <NeoML/Dnn/Dnn.h>
+#include <NeoML/Dnn/DnnDistributed.h>
 #include <NeoML/Dnn/Layers/BaseInPlaceLayer.h>
 #include <NeoML/Dnn/Layers/SourceLayer.h>
 #include <NeoML/Dnn/Layers/SinkLayer.h>
