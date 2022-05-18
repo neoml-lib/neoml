35c5dcbe5d97cbd5a33e4d72a20798a45d5af57e
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2022-05-18 17:02:36 +0300

    Remove data type check in CTransformLayer::BackwardOnce
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoML/src/Dnn/Layers/TransformLayer.cpp
+++ NeoML/src/Dnn/Layers/TransformLayer.cpp
@@ -160,7 +160,6 @@ void CTransformLayer::RunOnce()
 
 void CTransformLayer::BackwardOnce()
 {
-	NeoAssert( inputBlobs[0]->GetDataType() == CT_Float );
 	if( inputDiffBlobs[0]->GetData() != outputDiffBlobs[0]->GetData() ) {
 		MathEngine().VectorCopy( inputDiffBlobs[0]->GetData(), outputDiffBlobs[0]->GetData(),
 			inputDiffBlobs[0]->GetDataSize() );
