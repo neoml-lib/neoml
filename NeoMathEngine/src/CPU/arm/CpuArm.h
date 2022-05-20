7239220ab08734bc0bd0563774f07df12f8ee29b
sansanch5 <a.a.pakhomoff@gmail.com>
2022-05-20 20:33:50 +0300

    try-fix
    Signed-off-by: sansanch5 <a.a.pakhomoff@gmail.com>

--- NeoMathEngine/src/CPU/arm/CpuArm.h
+++ NeoMathEngine/src/CPU/arm/CpuArm.h
@@ -593,6 +593,81 @@ private:
 	const float32x4_t MinVal;
 };
 
+#define c_exp_hi 88.3762626647949f
+#define c_exp_lo -88.3762626647949f
+
+#define c_cephes_LOG2EF 1.44269504088896341
+#define c_cephes_exp_C1 0.693359375
+#define c_cephes_exp_C2 -2.12194440e-4
+
+#define c_cephes_exp_p0 1.9875691500E-4
+#define c_cephes_exp_p1 1.3981999507E-3
+#define c_cephes_exp_p2 8.3334519073E-3
+#define c_cephes_exp_p3 4.1665795894E-2
+#define c_cephes_exp_p4 1.6666665459E-1
+#define c_cephes_exp_p5 5.0000001201E-1
+
+/* exp() computed for 4 float at once */
+float32x4_t exp_ps( float32x4_t x ) {
+  float32x4_t tmp, fx;
+
+  float32x4_t one = vdupq_n_f32(1);
+  x = vminq_f32(x, vdupq_n_f32(c_exp_hi));
+  x = vmaxq_f32(x, vdupq_n_f32(c_exp_lo));
+
+  /* express exp(x) as exp(g + n*log(2)) */
+  fx = vmlaq_f32(vdupq_n_f32(0.5f), x, vdupq_n_f32(c_cephes_LOG2EF));
+
+  /* perform a floorf */
+  tmp = vcvtq_f32_s32(vcvtq_s32_f32(fx));
+
+  /* if greater, substract 1 */
+  uint32x4_t mask = vcgtq_f32(tmp, fx);
+  mask = vandq_u32(mask, vreinterpretq_u32_f32(one));
+
+
+  fx = vsubq_f32(tmp, vreinterpretq_f32_u32(mask));
+
+  tmp = vmulq_f32(fx, vdupq_n_f32(c_cephes_exp_C1));
+  float32x4_t z = vmulq_f32(fx, vdupq_n_f32(c_cephes_exp_C2));
+  x = vsubq_f32(x, tmp);
+  x = vsubq_f32(x, z);
+
+  static const float cephes_exp_p[6] = { c_cephes_exp_p0, c_cephes_exp_p1, c_cephes_exp_p2, c_cephes_exp_p3, c_cephes_exp_p4, c_cephes_exp_p5 };
+  float32x4_t y = vld1q_dup_f32(cephes_exp_p+0);
+  float32x4_t c1 = vld1q_dup_f32(cephes_exp_p+1);
+  float32x4_t c2 = vld1q_dup_f32(cephes_exp_p+2);
+  float32x4_t c3 = vld1q_dup_f32(cephes_exp_p+3);
+  float32x4_t c4 = vld1q_dup_f32(cephes_exp_p+4);
+  float32x4_t c5 = vld1q_dup_f32(cephes_exp_p+5);
+
+  y = vmulq_f32(y, x);
+  z = vmulq_f32(x,x);
+  y = vaddq_f32(y, c1);
+  y = vmulq_f32(y, x);
+  y = vaddq_f32(y, c2);
+  y = vmulq_f32(y, x);
+  y = vaddq_f32(y, c3);
+  y = vmulq_f32(y, x);
+  y = vaddq_f32(y, c4);
+  y = vmulq_f32(y, x);
+  y = vaddq_f32(y, c5);
+
+  y = vmulq_f32(y, z);
+  y = vaddq_f32(y, x);
+  y = vaddq_f32(y, one);
+
+  /* build 2^n */
+  int32x4_t mm;
+  mm = vcvtq_s32_f32(fx);
+  mm = vaddq_s32(mm, vdupq_n_s32(0x7f));
+  mm = vshlq_n_s32(mm, 23);
+  float32x4_t pow2n = vreinterpretq_f32_s32(mm);
+
+  y = vmulq_f32(y, pow2n);
+  return y;
+}
+
 // exponent
 // We use a polynomial approximation exp(x) of 7 degree over the [-ln(2), ln(2)] interval with the Remez method
 // The approximation uses Sollya 6.0 (http://sollya.gforge.inria.fr/)
@@ -639,10 +714,11 @@ public:
 	// Calculate the exponent with saturation
 	float32x4_t Execute(const float32x4_t& x) const
 	{
-		uint32x4_t maskMin = vcltq_f32(x, MinLog);
-		uint32x4_t maskMax = vcgtq_f32(x, MaxLog);
-		float32x4_t result = ExecuteNoCheck(x);
-		return Condition2Neon(maskMin, maskMax, MinResult, MaxResult, result);
+		//uint32x4_t maskMin = vcltq_f32(x, MinLog);
+		//uint32x4_t maskMax = vcgtq_f32(x, MaxLog);
+		//float32x4_t result = ExecuteNoCheck(x);
+		//return Condition2Neon(maskMin, maskMax, MinResult, MaxResult, result);
+		return exp_ps( x );
 	}
 
 private:
