00966ecbf7b23aadd7687abbca3d8b2b11f50135
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2022-06-22 01:05:53 +0300

    Enable distributed tests only for Windows and Linux
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoML/test/src/DnnDistributedTest.cpp
+++ NeoML/test/src/DnnDistributedTest.cpp
@@ -16,6 +16,8 @@ limitations under the License.
 #include <common.h>
 #pragma hdrstop
 
+#if FINE_PLATFORM( FINE_WINDOWS ) || FINE_PLATFORM( FINE_LINUX )
+
 #include <TestFixture.h>
 
 #ifdef NEOML_USE_OMP
@@ -215,3 +217,5 @@ TEST( CDnnDistributedTest, DnnDistributedSerializeTest )
 }
 
 #endif // NEOML_USE_OMP
+
+#endif // FINE_PLATFORM( FINE_WINDOWS ) || FINE_PLATFORM( FINE_LINUX )
