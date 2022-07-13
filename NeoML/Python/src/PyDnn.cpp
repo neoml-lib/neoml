ef5d44960f03aec82accf735f232866a2c0a0c40
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2022-07-13 19:54:49 +0300

    Add CLogLayer to Python wrapper
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoML/Python/src/PyDnn.cpp
+++ NeoML/Python/src/PyDnn.cpp
@@ -88,6 +88,7 @@ REGISTER_NEOML_PYLAYER_EX( "Activation", "HardSigmoid", "FmlCnnSigmoidTanhLayer"
 REGISTER_NEOML_PYLAYER_EX( "Activation", "HSwish", "FmlCnnHSwishLayer" )
 REGISTER_NEOML_PYLAYER_EX( "Activation", "Power", "FmlCnnPowerLayer" )
 REGISTER_NEOML_PYLAYER_EX( "Activation", "Exp", "NeoMLDnnExpLayer" )
+REGISTER_NEOML_PYLAYER_EX( "Activation", "Log", "NeoMLDnnLogLayer" )
 REGISTER_NEOML_PYLAYER( "RleConv", "FmlCnnRleConvLayer" )
 REGISTER_NEOML_PYLAYER_EX( "Pooling", "MaxPooling", "FmlCnnMaxPoolingLayer" )
 REGISTER_NEOML_PYLAYER_EX( "Pooling", "MeanPooling", "FmlCnnMeanPoolingLayer" )
