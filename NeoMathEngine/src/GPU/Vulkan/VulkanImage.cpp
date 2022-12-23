f7fd7d11ff8a59df76f74b9c214b65e773e6ce74
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2022-12-23 18:48:03 +0100

    Fix compilation without UNITY build (#780)
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoMathEngine/src/GPU/Vulkan/VulkanImage.cpp
+++ NeoMathEngine/src/GPU/Vulkan/VulkanImage.cpp
@@ -125,8 +125,8 @@ bool CVulkanImage::IsImageFit(int& newWidth, int& newHeight) const
 		return true;
 	}
 
-	newWidth = max(newWidth, width);
-	newHeight = max(newHeight, height);
+	newWidth = std::max<int>(newWidth, width);
+	newHeight = std::max<int>(newHeight, height);
 
 	return false;
 }
