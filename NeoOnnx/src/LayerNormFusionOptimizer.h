88df0222df89389f8b78942dedb717ddfd76eb40
Kirill Golikov <kirill.golikov@abbyy.com>
2022-12-28 18:17:28 +0100

    [NeoOnnx] LayerNormFusionOptimizer minor comment fix
    Signed-off-by: Kirill Golikov <kirill.golikov@abbyy.com>

--- NeoOnnx/src/LayerNormFusionOptimizer.h
+++ NeoOnnx/src/LayerNormFusionOptimizer.h
@@ -19,65 +19,64 @@ limitations under the License.
 
 namespace NeoOnnx {
 
-/*  Layer Normalization will fuse ObjectLayerNormalization into one layer:
- *
- *  (x - mean(x, axis)) / sqrt(var(x, axis)) * scale + bias  , where 'x' is the input and var(x) = mean((x-mean)^2).
- *
- *  +---------------------+
- *  |                     |
- *  |                     v
- *  X --> ReduceMean --> Sub --> Pow --> ReduceMean --> Add --> Sqrt --> Div --> Mul --> Add
- *                        |                                               ^
- *                        |                                               |
- *                        +-----------------------------------------------+
- *  It also handles cases of duplicated sub layers exported from older version of PyTorch :
- *  +---------------------+
- *  |                     v
- *  |          +-------> Sub ---------------------------------------------+
- *  |          |                                                          |
- *  |          |                                                          v
- *  X --> ReduceMean --> Sub --> Pow --> ReduceMean --> Add --> Sqrt --> Div --> Mul --> Add
- *  |                     ^
- *  |                     |
- *  +---------------------+
- *
- *  In recent pytorch, Cast layers may be inserted before Pow to ensure that both inputs 'base' and 'power' are the same type
- *  due to restriction in older opsets. Therefore, Layer Normalization will also handle the case below :
- *  +---------------------+
- *  |                     |
- *  |                     v
- *  X --> ReduceMean --> Sub --> Cast --> Pow --> ReduceMean --> Add --> Sqrt --> Div --> Mul --> Add
- *                        |                                                        ^
- *                        |                                                        |
- *                        +--------------------------------------------------------+
- *  +---------------------+       Cast
- *  |                     |        |
- *  |                     v        v
- *  X --> ReduceMean --> Sub -->  Pow --> ReduceMean --> Add --> Sqrt --> Div --> Mul --> Add
- *                        |                                                ^
- *                        |                                                |
- *                        +------------------------------------------------+
- *
- *  When using Apex O2, a Cast layer may be inserted between Div and Mul, Layer Normalization will also handle the case below:
- *  +---------------------+
- *  |                     |
- *  |                     v
- *  X --> ReduceMean --> Sub --> Pow --> ReduceMean --> Add --> Sqrt --> Div --> Cast --> Mul --> Add
- *                        |                                               ^
- *                        |                                               |
- *                        +-----------------------------------------------+
- *  OR
- *           +---------------------+
- *           |                     |
- *           |                     v
- *  X --> Cast --> ReduceMean --> Sub --> Pow --> ReduceMean --> Add --> Sqrt --> Div --> Cast --> Mul --> Add
- *                                 |                                               ^
- *                                 |                                               |
- *                                 +-----------------------------------------------+
- *  Logically since LayerNormalization supports input and scale/bias in different data types, and during the kernel execution,
- *  data are casted to float/double to calculate for precision, so if there is any Cast Ops in the sub-graph, we can remove it.
- *  Such Cast Op can be the input of the sub-graph, or an Cast Op between the Div and Mul layers.
- */
+//  Layer Normalization will fuse ObjectLayerNormalization into one layer:
+// 
+//  (x - mean(x, axis)) / sqrt(var(x, axis)) * scale + bias  , where 'x' is the input and var(x) = mean((x-mean)^2).
+// 
+//  +---------------------+
+//  |                     |
+//  |                     v
+//  X --> ReduceMean --> Sub --> Pow --> ReduceMean --> Add --> Sqrt --> Div --> Mul --> Add
+//                        |                                               ^
+//                        |                                               |
+//                        +-----------------------------------------------+
+//  It also handles cases of duplicated sub layers exported from older version of PyTorch :
+//  +---------------------+
+//  |                     v
+//  |          +-------> Sub ---------------------------------------------+
+//  |          |                                                          |
+//  |          |                                                          v
+//  X --> ReduceMean --> Sub --> Pow --> ReduceMean --> Add --> Sqrt --> Div --> Mul --> Add
+//  |                     ^
+//  |                     |
+//  +---------------------+
+// 
+//  In recent pytorch, Cast layers may be inserted before Pow to ensure that both inputs 'base' and 'power' are the same type
+//  due to restriction in older opsets. Therefore, Layer Normalization will also handle the case below :
+//  +---------------------+
+//  |                     |
+//  |                     v
+//  X --> ReduceMean --> Sub --> Cast --> Pow --> ReduceMean --> Add --> Sqrt --> Div --> Mul --> Add
+//                        |                                                        ^
+//                        |                                                        |
+//                        +--------------------------------------------------------+
+//  +---------------------+       Cast
+//  |                     |        |
+//  |                     v        v
+//  X --> ReduceMean --> Sub -->  Pow --> ReduceMean --> Add --> Sqrt --> Div --> Mul --> Add
+//                        |                                                ^
+//                        |                                                |
+//                        +------------------------------------------------+
+// 
+//  When using Apex O2, a Cast layer may be inserted between Div and Mul, Layer Normalization will also handle the case below:
+//  +---------------------+
+//  |                     |
+//  |                     v
+//  X --> ReduceMean --> Sub --> Pow --> ReduceMean --> Add --> Sqrt --> Div --> Cast --> Mul --> Add
+//                        |                                               ^
+//                        |                                               |
+//                        +-----------------------------------------------+
+//  OR
+//           +---------------------+
+//           |                     |
+//           |                     v
+//  X --> Cast --> ReduceMean --> Sub --> Pow --> ReduceMean --> Add --> Sqrt --> Div --> Cast --> Mul --> Add
+//                                 |                                               ^
+//                                 |                                               |
+//                                 +-----------------------------------------------+
+//  Logically since LayerNormalization supports input and scale/bias in different data types, and during the kernel execution,
+//  data are casted to float/double to calculate for precision, so if there is any Cast Ops in the sub-graph, we can remove it.
+//  Such Cast Op can be the input of the sub-graph, or an Cast Op between the Div and Mul layers.
 class CLayerNormFusionOptimizer final : public IOptimizer {
 public:
 	static const char* const classesOfSkipLayers[];
