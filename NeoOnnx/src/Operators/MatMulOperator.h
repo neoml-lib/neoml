261d0050cb700b4dc6ac6b5291ef7b7ce55286ee
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2023-01-09 14:12:34 +0300

    Restore CMatMulOperator
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoOnnx/src/Operators/MatMulOperator.h
+++ NeoOnnx/src/Operators/MatMulOperator.h
@@ -15,8 +15,6 @@ limitations under the License.
 
 #pragma once
 
-#ifdef TODO
-
 #include "../LayerOperator.h"
 
 namespace NeoOnnx {
@@ -31,10 +29,7 @@ protected:
 	void AddLayers( const CTensorArray& inputs, CDnn& dnn, CTensorArray& outputs ) const override;
 
 private:
-	CPtr<const CUserTensor> prepareTensor( const CTensorBase& tensor, bool isFirstArg,
-		bool mergeBatchWithHeight, CDnn& dnn ) const;
+	CPtr<const CUserTensor> prepareTensor( const CTensorBase& tensor, bool isFirstArg, CDnn& dnn ) const;
 };
 
 } // namespace NeoOnnx
-
-#endif
