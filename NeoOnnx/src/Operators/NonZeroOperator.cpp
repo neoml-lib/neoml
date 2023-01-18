74c69fc9bce7c0b26e80a9fbea69c501f3ed4e72
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2023-01-19 02:49:47 +0300

    Add workaround for ConstantOfShape + NonZero
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoOnnx/src/Operators/NonZeroOperator.cpp
+++ NeoOnnx/src/Operators/NonZeroOperator.cpp
@@ -22,8 +22,23 @@ limitations under the License.
 #include "NeoOnnxCheck.h"
 
 #include <NeoML/Dnn/Layers/Onnx/OnnxNonZeroLayer.h>
+#include <NeoML/Dnn/Layers/Onnx/OnnxConstantOfShapeLayer.h>
+#include <NeoML/Dnn/Layers/Onnx/OnnxRangeLayer.h>
+#include <NeoML/Dnn/Layers/Onnx/OnnxSourceHelper.h>
 
 namespace NeoOnnx {
+
+static CPtr<const CShapeTensor> addNonZeroOpSourceHelper( const CString& name, CDnn& dnn, int value )
+{
+	CPtr<COnnxSourceHelper> source = new COnnxSourceHelper( dnn.GetMathEngine() );
+	source->SetName( name );
+	source->Blob() = CDnnBlob::CreateDataBlob( dnn.GetMathEngine(), CT_Int, 1, 1, 1 );
+	source->Blob()->Fill<int>( value );
+	dnn.AddLayer( *source );
+	return new CShapeTensor( CTensorLayout( { BD_BatchLength } ), CTensorShape( { 1 } ), CLayerOutput( source, 0 ) );
+}
+
+//---------------------------------------------------------------------------------------------------------------------
 
 CNonZeroOperator::CNonZeroOperator( const onnx::NodeProto& nonZero, int opsetVersion ) :
 	CLayerOperator( nonZero, opsetVersion )
@@ -37,7 +52,43 @@ CNonZeroOperator::CNonZeroOperator( const onnx::NodeProto& nonZero, int opsetVer
 void CNonZeroOperator::AddLayers( const CTensorArray& inputs, CDnn& dnn, CTensorArray& outputs ) const
 {
 	CheckNoNullInputs( inputs );
-	CheckNeoOnnxSupport( inputs[0]->Type() != TTensorType::User, "user-provided data", *this );
+
+	if( inputs[0]->Type() == TTensorType::User ) {
+		const CUserTensor& input = dynamic_cast<const CUserTensor&>( *inputs[0] );
+		COnnxConstantOfShapeLayer* constantOfShapeLayer = dynamic_cast<COnnxConstantOfShapeLayer*>( input.Layer() );
+		CheckNeoOnnxSupport( constantOfShapeLayer != nullptr,
+			"NonZero operator supports user input only from ConstantOfShape", *this );
+		if( constantOfShapeLayer->GetValue().GetDataType() == CT_Float ) {
+			CheckNeoOnnxSupport( constantOfShapeLayer->GetValue().GetData().GetValue() != 0,
+				"NonZero operator supprots user input only with ConstantOfShape( value != 0 )", *this );
+		} else {
+			CheckNeoOnnxSupport( constantOfShapeLayer->GetValue().GetData<int>().GetValue() != 0,
+				"NonZero operator supprots user input only with ConstantOfShape( value != 0 )", *this );
+		}
+		// Sink in order to avoid hanging output
+		Sink( constantOfShapeLayer, Name() + "_SafeSink" );
+
+		// Some exporters use 1-dimensional ConstantOfShape + NonZero which is equivalent of Range
+		CPtr<COnnxRangeLayer> rangeLayer = new COnnxRangeLayer( dnn.GetMathEngine() );
+		rangeLayer->SetName( Name() );
+		
+		CPtr<const CShapeTensor> startTensor = addNonZeroOpSourceHelper( Name() + "_Start", dnn, 0 );
+		rangeLayer->Connect( 0, *startTensor->Layer(), startTensor->OutputIndex() );
+
+		rangeLayer->Connect( 1, constantOfShapeLayer->GetInputName( 0 ),
+			constantOfShapeLayer->GetInputOutputNumber( 0 ) );
+
+		CPtr<const CShapeTensor> deltaTensor = addNonZeroOpSourceHelper( Name() + "_Delta", dnn, 1 );
+		rangeLayer->Connect( 2, *deltaTensor->Layer(), deltaTensor->OutputIndex() );
+
+		dnn.AddLayer( *rangeLayer );
+
+		// COnnxRangeLayer will return the indices of non-zero elements in BD_BatchLength (other dims will be 1)
+		// But NonZero specification requires 2-dimensional output of Nx1 
+		outputs.Add( new CUserTensor( CTensorLayout( { BD_BatchLength, BD_BatchWidth } ),
+			CLayerOutput( rangeLayer, 0 ) ) );
+		return;
+	}
 
 	CPtr<COnnxNonZeroLayer> nonZeroLayer = new COnnxNonZeroLayer( dnn.GetMathEngine() );
 	nonZeroLayer->SetName( Name() );
