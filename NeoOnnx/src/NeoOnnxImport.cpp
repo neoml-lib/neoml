a23bcbca3a10f06bca00805b1fcb76799d4bb3e4
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2023-01-20 02:29:54 +0300

    Revert "[NeoOnnx] Add CDnnOptimizer (#775)" (#789)
    This reverts commit 79148666115ffa441264f65d63d0c10e7638b443.
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>
    
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoOnnx/src/NeoOnnxImport.cpp
+++ NeoOnnx/src/NeoOnnxImport.cpp
@@ -29,9 +29,6 @@ limitations under the License.
 #include "GraphInput.h"
 #include "GraphOutput.h"
 
-#include "LayerNormFusionOptimizer.h"
-#include "DnnOptimizer.h"
-
 namespace NeoOnnx {
 
 // Checks if all of the operators are supported by NeoOnnx
@@ -209,8 +206,6 @@ void LoadFromOnnx( const char* fileName, const CImportSettings& importSettings,
 		buildDnnFromGraphProto( model.graph(), getOpsetVersion( model ), importSettings,
 			dnn, info.Inputs, info.Outputs );
 		extractMetadata( model, info.Metadata );
-		CDnnOptimizer dnnOptimizer( dnn );
-		dnnOptimizer.Optimize();
 	} catch( ... ) {
 		input.close();
 		google::protobuf::ShutdownProtobufLibrary();
@@ -234,11 +229,10 @@ void LoadFromOnnx( const void* buffer, int bufferSize, const CImportSettings& im
 		if( !model.ParseFromString( strBuffer ) ) {
 			NeoOnnxCheck( false, "Failed to parse model from buffer" );
 		}
+
 		buildDnnFromGraphProto( model.graph(), getOpsetVersion( model ), importSettings,
 			dnn, info.Inputs, info.Outputs );
 		extractMetadata( model, info.Metadata );
-		CDnnOptimizer dnnOptimizer( dnn );
-		dnnOptimizer.Optimize();
 	} catch( ... ) {
 		google::protobuf::ShutdownProtobufLibrary();
 		throw;
@@ -247,5 +241,4 @@ void LoadFromOnnx( const void* buffer, int bufferSize, const CImportSettings& im
 	google::protobuf::ShutdownProtobufLibrary();
 }
 
-} //NeoOnnx
-
+}
