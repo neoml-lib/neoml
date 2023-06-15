7811e2e489d30bf1f7455e485c263b34f7fa06d4
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2023-06-15 03:13:47 +0200

    Fix formatting
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoML/src/Dnn/Optimization/MobileNetV3Optimizer.cpp
+++ NeoML/src/Dnn/Optimization/MobileNetV3Optimizer.cpp
@@ -129,7 +129,8 @@ bool CMobileNetV3Optimizer::detectMNv3Residual( CBaseLayer& residual, CMNv3Block
 
 		CLayerOutput<> blockData = graph.GetConnectedOutput<>( residual, 1 - i );
 		if( downConv != nullptr && detectMNv3NonResidual( *downConv, detectedBlock )
-			&& blockData == detectedBlock.InputData && graph.GetConnectedInputsCount( *downConv, 0 ) == 1 )
+			&& graph.GetConnectedInputsCount( *downConv, 0 ) == 1
+			&& blockData == detectedBlock.InputData )
 		{
 			detectedBlock.Residual = &residual;
 			graph.SelectLayer( residual );
