c8b2d9fc996f9cc4f099c0c39c291c740343b1a8
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2023-06-20 17:55:31 +0200

    Fix formatting in files related to distributed training (#872)
    * Fix formatting in files related to distributed training
    
    Signed-off-by: Valery Fedyunin <valery.fedyunin@abbyy.com>
    
    * Typo fix
    
    Signed-off-by: Valery Fedyunin <valery.fedyunin@abbyy.com>
    
    ---------
    
    Signed-off-by: Valery Fedyunin <valery.fedyunin@abbyy.com>

--- NeoMathEngine/src/CPU/CpuMathEngineDnnDistributed.h
+++ NeoMathEngine/src/CPU/CpuMathEngineDnnDistributed.h
@@ -1,8 +1,10 @@
-/* Copyright © 2017-2020 ABBYY Production LLC
+/* Copyright © 2017-2023 ABBYY
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
 	http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,22 +23,22 @@ namespace NeoML {
 
 class CMultiThreadDistributedCommunicator {
 public:
-    explicit CMultiThreadDistributedCommunicator( int n_threads );
-    void AllReduce( const CFloatHandle& handle, int size );
-    void Broadcast( const CFloatHandle& handle, int size, int root );
-    void Abort() { isAbort = true; }
+	explicit CMultiThreadDistributedCommunicator( int n_threads );
+	void AllReduce( const CFloatHandle& handle, int size );
+	void Broadcast( const CFloatHandle& handle, int size, int root );
+	void Abort() { isAbort = true; }
 private:
-    std::vector<float*> handles;
+	std::vector<float*> handles;
 
-    std::atomic<int> counter;
-    std::atomic<int> waiting_flag;
-    int n_threads;
-    std::atomic<bool> isAbort;
+	std::atomic<int> counter;
+	std::atomic<int> waiting_flag;
+	int n_threads;
+	std::atomic<bool> isAbort;
 
-    // non-blocking barrier
-    void barrier();
-    // collects handles from all threads into the common array
-    void collectHandles( const CFloatHandle& handle );
+	// non-blocking barrier
+	void barrier();
+	// collects handles from all threads into the common array
+	void collectHandles( const CFloatHandle& handle );
 };
 
 } // namespace NeoML
