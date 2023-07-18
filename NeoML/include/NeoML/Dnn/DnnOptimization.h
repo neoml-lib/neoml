d1f0d2408a795ff5269f36f3e024d5f75142b236
Valeriy Fedyunin <valery.fedyunin@abbyy.com>
2023-07-18 23:38:11 +0200

    Update comment
    Signed-off-by: Valeriy Fedyunin <valery.fedyunin@abbyy.com>

--- NeoML/include/NeoML/Dnn/DnnOptimization.h
+++ NeoML/include/NeoML/Dnn/DnnOptimization.h
@@ -42,7 +42,7 @@ struct NEOML_API CDnnOptimizationReport {
 	int MobileNetV3NonResidualBlocks = 0;
 	// Number of optimized MobileNetV3 blocks with residual connection
 	int MobileNetV3ResidualBlocks = 0;
-	// Number of chains of rowwise operations (0 if CDnnOptimizationSettings::OptimizeRowwiseChains is false)
+	// Number of chains of rowwise operations
 	int RowwiseChainCount = 0;
 };
 
