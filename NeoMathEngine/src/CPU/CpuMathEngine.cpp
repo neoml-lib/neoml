48af966d0051f24aea1a9f58ad1f27c5fee5606c
Valerii Fediunin <valery.fedyunin@abbyy.com>
2023-09-05 17:29:48 +0200

    Update comment
    Signed-off-by: Valerii Fediunin <valery.fedyunin@abbyy.com>

--- NeoMathEngine/src/CPU/CpuMathEngine.cpp
+++ NeoMathEngine/src/CPU/CpuMathEngine.cpp
@@ -66,7 +66,7 @@ CCpuMathEngine::CCpuMathEngine( size_t _memoryLimit,
 #ifdef NEOML_USE_AVX
 	if( dllLoader.IsLoaded( CDllLoader::AVX_DLL ) ) {
 		simdMathEngine = std::unique_ptr<ISimdMathEngine>( CDllLoader::avxDll->CreateSimdMathEngine( this ) );
-		// Don't use custom sgemm function when we are compiled with MKL.
+		// Don't use custom sgemm function when we are compiled with MKL or MLAS.
 #if !defined( NEOML_USE_MKL ) && !defined( NEOML_USE_MLAS )
 		customSgemmFunction = simdMathEngine->GetSgemmFunction();
 #endif // !defined( NEOML_USE_MKL ) && !defined( NEOML_USE_MLAS )
