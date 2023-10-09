b58ca67104c69bcb86c1cc4c1456bb33ea6609f2
Valery Fedyunin <valery.fedyunin@abbyy.com>
2023-10-09 14:52:23 +0200

    Fix clang compilation
    Signed-off-by: Valery Fedyunin <valery.fedyunin@abbyy.com>

--- cmake/Settings.cmake
+++ cmake/Settings.cmake
@@ -66,12 +66,17 @@ function(configure_target TARGET_NAME)
             $<$<COMPILE_LANGUAGE:CXX>:-Wall>
             $<$<COMPILE_LANGUAGE:CXX>:-Wextra>
             $<$<COMPILE_LANGUAGE:CXX>:-Wpedantic>
-            $<$<COMPILE_LANGUAGE:CXX>:-Wno-deprecated-copy>
             $<$<COMPILE_LANGUAGE:CXX>:-Wno-deprecated-declarations>
             $<$<COMPILE_LANGUAGE:CXX>:-Wno-unused-value>
             $<$<COMPILE_LANGUAGE:CXX>:-Wno-unknown-pragmas>
             $<$<COMPILE_LANGUAGE:CXX>:-Wno-strict-overflow>
         )
+        if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+            # This option is unknown to clang
+            target_compile_options(${TARGET_NAME} PRIVATE
+                $<$<COMPILE_LANGUAGE:CXX>:-Wno-deprecated-copy>
+            )
+        endif()
     endif()
     
     # No extensions use
