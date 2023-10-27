ba4c37870ae03b62e39acf3f3c4966d6604f98d3
Valerii Fediunin <valery.fedyunin@abbyy.com>
2023-10-28 01:32:56 +0200

    Add comment for NeoOnnx Python API
    Signed-off-by: Valerii Fediunin <valery.fedyunin@abbyy.com>

--- NeoML/Python/neoml/Onnx/API.py
+++ NeoML/Python/neoml/Onnx/API.py
@@ -19,11 +19,22 @@ import neoml.Dnn
 
 
 def _validate_layouts(layouts):
+    """Checks whether layouts is an instance of expected class.
+    Raises ValueError if something is wrong
+
+    :param layouts: dict where each key is a str (input/output names) and each value is a list (blob dims)
+        None means default layout
+    :type layouts: dict(str: list[str]) or None
+    """
     if layouts is None:
         return  # Everything is OK, default behavior
+    if not isinstance(layouts, dict):
+        raise ValueError('layouts must be dict')
     for name, layout in layouts.items():
         if not isinstance(name, str):
             raise ValueError('name must be a str')
+        if not isinstance(layout, list):
+            raise ValueError('each layout muust be a list')
         rem_dims = {'batch_length', 'batch_width', 'list_size', 'height', 'width', 'depth', 'channels'}
         for dim in layout:
             if not dim in rem_dims:
@@ -32,6 +43,23 @@ def _validate_layouts(layouts):
 
 
 def load_from_file(file_name, math_engine, input_layouts=None, output_layouts=None):
+    """Loads ONNX model from the given file
+    Returns NeoML net and dictionary with additional info (input/output names, metadata, etc.)
+
+    :param file_name: name of file to load from
+    :type file_name: str
+
+    :param math_engine: math engine to use in model
+    :type math_engine: neoml.MathEngine.MathEngine
+
+    :param input_layouts: custom layouts of ONNX model inputs. For more details see ONNX.ipynb tutorial.
+    :type input_layouts: dict(str: list[str]) or None
+
+    :param output_layouts: custom layouts of ONNX model outputs. For more details see ONNX.ipynb tutorial.
+    :type output_layouts: dict(str: list[str]) or None
+
+    :return: tuple(neoml.Dnn.Dnn, dict)
+    """
     _validate_layouts(input_layouts)
     _validate_layouts(output_layouts)
     dnn = neoml.Dnn.Dnn(math_engine)
@@ -40,6 +68,23 @@ def load_from_file(file_name, math_engine, input_layouts=None, output_layouts=No
 
 
 def load_from_buffer(buffer, math_engine, input_layouts=None, output_layouts=None):
+    """Loads ONNX model from the memory buffer
+    Returns NeoML net and dictionary with additional info (input/output names, metadata, etc.)
+
+    :param buffer: raw data to load from
+    :type buffer: str
+
+    :param math_engine: math engine to use in model
+    :type math_engine: neoml.MathEngine.MathEngine
+
+    :param input_layouts: custom layouts of ONNX model inputs. For more details see ONNX.ipynb tutorial.
+    :type input_layouts: dict(str: list[str]) or None
+
+    :param output_layouts: custom layouts of ONNX model outputs. For more details see ONNX.ipynb tutorial.
+    :type output_layouts: dict(str: list[str]) or None
+
+    :return: tuple(neoml.Dnn.Dnn, dict)
+    """
     _validate_layouts(input_layouts)
     _validate_layouts(output_layouts)
     dnn = neoml.Dnn.Dnn(math_engine)
