eafb73a175778e7b1806442824d9917606192d09
Kirill Golikov <kirill.golikov@abbyy.com>
2024-06-20 11:24:01 +0200

    [NeoML] Fix bug in CEltwiseDivLayer, add tests for backward propagation EltwiseOpLayers (#1072)
    Signed-off-by: Kirill Golikov <kirill.golikov@abbyy.com>

--- NeoML/test/src/DnnLayersSerializationTest.cpp
+++ NeoML/test/src/DnnLayersSerializationTest.cpp
@@ -1,4 +1,4 @@
-/* Copyright © 2021-2023 ABBYY
+/* Copyright © 2021-2024 ABBYY
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -46,6 +46,8 @@ GTEST_TEST( LayerSerialization, CheckRegisteredLayers )
 	// Layers from other tests
 	ignoredLayers.Add( "CConvActivationTestLayer" ); // from UnpackCompositeOptimizerTest
 	ignoredLayers.Add( "CTwoConvActivationTestLayer" ); // from UnpackCompositeOptimizerTest
+	ignoredLayers.Add( "NeoMLDnnSimpleTestDummyLearningLayer" ); // from CDnnSimpleTest
+	ignoredLayers.Add( "NeoMLDnnSimpleTestDummyLossLayer" ); // from CDnnSimpleTest
 
 	CArray<const char*> layerClasses;
 	GetRegisteredLayerClasses( layerClasses );
